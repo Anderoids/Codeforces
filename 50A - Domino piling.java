dccb2199c38e1d2a05d8ada96892ec4bfcecad45
Guduru Manasa <114640857+Anderoids@users.noreply.github.com>
2025-06-29 19:12:32 +0530

    Create 50A - Domino piling.java

---
+++ 50A - Domino piling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+public class Main{
+    public static void main(String[]args){ 
+        Scanner sc=new Scanner(System.in); 
+        int m=sc.nextInt(); 
+        int n=sc.nextInt();
+        int mxn=m*n;
+        double div=mxn/2; 
+System.out.println((int)Math.floor(div)); 
+        
+    }
+    }
